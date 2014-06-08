e41ff544d837bf3050925de35ff2d78bee0104d4
PsyhoLord <psyholord@gmail.com>
2014-06-08 20:17:45 +0300

    First Start
    I made Main()

--- BoardGames/BoardGames/MainForm.cpp
+++ BoardGames/BoardGames/MainForm.cpp
@@ -1,2 +1,16 @@
 #include "MainForm.h"
 
+#include "MainForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThread]
+void main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	BoardGames::MainForm form;
+	Application::Run(%form);
+}
